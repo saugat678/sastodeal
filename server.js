21c0783cc1fd33b79b480387e4e677279ea5e13e
saugat678 <128444102+saugat678@users.noreply.github.com>
2023-06-19 07:19:18 +0545

    Update server.js

--- server.js
+++ server.js
@@ -4,42 +4,40 @@ import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
-import cors from 'cors';
+import cors from "cors";
 import categoryRoute from "./routes/CategoryRoute.js";
 import ProductRoute from "./routes/ProductRoute.js";
-     import Path  from "path";
+import { fileURLToPath } from "url";
+import path from "path";
 
-improt {fileURL ToPath} from "url";
-
-
-
-// configure . env
+// configure .env
 dotenv.config();
 
 // rest object
 const app = express();
 // database
 connectDB();
-const_filename = fileURLToPath (import.meta.url);
-cost_dirname = path.dirname(_filename);
-// middelwares
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// middlewares
 app.use(cors());
-app.use (express.json());
-app.use(morgan('dev'));
- app.use(express.static(Path.join(__dirname,'./client/build')));
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "./client/build")));
+
 // routes
 app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/category",categoryRoute);
+app.use("/api/v1/category", categoryRoute);
 app.use("/api/v1/product", ProductRoute);
 
-app.get("/",(req,res)=>{
-    res.sendfile(path.join(_dirname,"./client/build/index.html"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-
-// POrt
+// Port
 const PORT = process.env.PORT || 8080;
 // run listen
-app.listen(PORT,()=>{
-    console.log(`Server Runnig on ${PORT}`.bgCyan.white);
+app.listen(PORT, () => {
+  console.log(`Server Running on ${PORT}`.bgCyan.white);
 });
+
