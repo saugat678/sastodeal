2ff47c7d8a6461d0b904b14dd3166a103a7fd026
saugat678 <128444102+saugat678@users.noreply.github.com>
2023-06-19 07:53:31 +0545

    Update server.js

--- server.js
+++ server.js
@@ -31,7 +31,8 @@ app.use("/api/v1/category", categoryRoute);
 app.use("/api/v1/product", ProductRoute);
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+ res.sendFile(path.join(__dirname, "build", "index.html"));
+
 
 });
 
