7e95d8b00dcf6437e06ab09b201cd71ada8fbd5a
saugat678 <128444102+saugat678@users.noreply.github.com>
2023-06-19 06:57:59 +0545

    Update server.js
    vghfj

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ import authRoutes from "./routes/authRoute.js";
 import cors from 'cors';
 import categoryRoute from "./routes/CategoryRoute.js";
 import ProductRoute from "./routes/ProductRoute.js";
-// import Path  from "path";
+     import Path  from "path";
+
+improt {fileURL ToPath} from "url";
 
 
 
@@ -18,19 +20,20 @@ dotenv.config();
 const app = express();
 // database
 connectDB();
-
+const_filename = fileURLToPath (import.meta.url);
+cost_dirname = path.dirname(_filename);
 // middelwares
 app.use(cors());
 app.use (express.json());
 app.use(morgan('dev'));
-// app.use(express.static(Path.join(__dirname,'./client/build')));
+ app.use(express.static(Path.join(__dirname,'./client/build')));
 // routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category",categoryRoute);
 app.use("/api/v1/product", ProductRoute);
 
 app.get("/",(req,res)=>{
-    res.send("<h2>wecom to sastodeal app</h2>");
+    res.sendfile(path.join(_dirname,"./client/build/index.html"));
 });
 
 
